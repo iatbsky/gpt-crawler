72e1bd910ad432d75b9601af89b28c39aa557da5
Mei Yifan <64300296+iatbsky@users.noreply.github.com>
2023-11-30 14:28:45 +0800

    [skip actions]Update config.ts

--- config.ts
+++ config.ts
@@ -1,7 +1,7 @@
 import { Config } from "./src/config";
 
 export const defaultConfig: Config = {
-  url: "https://zh.minecraft.wiki",
+  url: "https://zh.minecraft.wiki/minecraft",
   match: "https://zh.minecraft.wiki/w/**",
   maxPagesToCrawl: 50,
   outputFileName: "output.json",
