c9607ee8f9e14293a3a21665304339c6d1efeebb
iatbsky <64300296+iatbsky@users.noreply.github.com>
2024-01-31 10:22:10 +0800

    Update config.ts
    mcsmanager

--- config.ts
+++ config.ts
@@ -1,8 +1,8 @@
 import { Config } from "./src/config";
 
 export const defaultConfig: Config = {
-  url: "https://zh.minecraft.wiki/minecraft",
-  match: "https://zh.minecraft.wiki/w/**",
+  url: "https://docs.mcsmanager.com/#/zh-cn/apis/",
+  match: "https://docs.mcsmanager.com/#/zh-cn/**",
   maxPagesToCrawl: 50,
   outputFileName: "output.json",
   maxTokens: 2000000,
