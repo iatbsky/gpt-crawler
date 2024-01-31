ae90f94711ea7d9d34dd1a97c864ea39fecb09ff
iatbsky <64300296+iatbsky@users.noreply.github.com>
2024-01-31 10:33:11 +0800

    Update config.ts

--- config.ts
+++ config.ts
@@ -1,8 +1,8 @@
 import { Config } from "./src/config";
 
 export const defaultConfig: Config = {
-  url: "https://docs.mcsmanager.com/#/zh-cn/apis/",
-  match: "https://docs.mcsmanager.com/#/zh-cn/**",
+  url: "https://docs.mcsmanager.com/#/zh-cn/",
+  match: "https://docs.mcsmanager.com/#/zh-cn/apis/**",
   maxPagesToCrawl: 50,
   outputFileName: "output.json",
   maxTokens: 2000000,
